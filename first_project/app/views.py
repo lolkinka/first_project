b747c59b4970c7036d071c2ceb279672467e002b
lolkinka <90253371+lolkinka@users.noreply.github.com>
2022-06-14 02:08:49 +0700

    Add files via upload

---
+++ first_project/app/views.py
@@ -0,0 +1,33 @@
+import os
+from datetime import datetime
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    rez_str = ''
+    rez = sorted(os.listdir())
+    for n in rez:
+        rez_str += n + "\n"
+    return HttpResponse(rez_str)
+
